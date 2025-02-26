a804075cc4f916607ba60c57dd749040a4ca6b5e
jorge <jresino1@kpmg.es>
2025-02-26 16:27:23 +0100

    cnserts

--- factiva_data/sql/create.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists clientes;
-
-create table clientes(
- id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (id) 
-);
-ALTER TABLE clientes AUTO_INCREMENT=1;
--- factiva_data/sql/insert.sql
+++ factiva_data/sql/insert.sql
@@ -1,3 +1,4 @@
 
 -- Insertar un cliente llamado "Jorge"
-INSERT INTO clientes (name) VALUES ('Jorge');
+INSERT INTO clientes (name) VALUES ('MAIZ');
+INSERT INTO clientes (name) VALUES ('OJEDA');
